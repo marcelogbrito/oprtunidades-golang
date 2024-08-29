d32ce1b0dce1ab127837a22368dd2748719ee4fb
Marcelo Brito <marcelogbrito@gmail.com>
2024-08-29 08:35:22 -0300

    feat: add routes logic

--- router/router.go
+++ router/router.go
@@ -5,11 +5,10 @@ import (
 )
 
 func Initialize() {
+	//Initialize router
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	// Initilize routes
+	initializeRoutes(r)
+	// listen and serve on 0.0.0.0:8080
+	r.Run()
 }
