2ac431e2f4ad8d19123359361b006227c9b1d2a0
Marcelo Brito <marcelogbrito@gmail.com>
2024-08-29 23:25:43 -0300

    feat: created logger

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/marcelogbrito/oprtunidades-golang/router"
+import (
+	"github.com/marcelogbrito/oprtunidades-golang/config"
+	"github.com/marcelogbrito/oprtunidades-golang/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+
+	logger = config.GetLogger("main")
+	// Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	//inicia o router
 	router.Initialize()
 }
