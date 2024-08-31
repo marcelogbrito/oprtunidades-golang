4d200ed7aa039445d0a1fab8a1f2ada616d11e18
Marcelo Brito <marcelogbrito@gmail.com>
2024-08-31 13:28:38 -0300

    feat: add request and response for CreateOpening

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -4,10 +4,35 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/marcelogbrito/oprtunidades-golang/schemas"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST Opening",
-	})
+	request := CreateOpeningRequest{}
+	// binding to populate
+	ctx.BindJSON(&request)
+	//validate
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+	//logger.Infof("request received: %+v", request)
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+	//creating in database
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("error creating openinh: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating opening")
+		return
+	}
+
+	sendSuccess(ctx, "create opening", opening)
 }
